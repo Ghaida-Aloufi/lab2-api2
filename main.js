a8d498aa73d553a9178754d5a3b2550f291f4b89
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-16 15:01:08 +0300

    add lab

--- main.js
+++ main.js
@@ -1,38 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let input = document.getElementById("input");
+  let imgInput = document.getElementById("img");
+  let submitBtn = document.getElementById("btn");
+  let container = document.getElementById("container");
 
+  let addImage = (id, url, name) => {
+    let div = document.createElement("div");
+    div.classList.add("image-item");
 
-let input = document.getElementById("input")
-let btn = document.getElementById("btn")
-let btn2 = document.getElementById("btn2")
-let img = document.getElementById("img")
-let Container = document.getElementById("container");
+    div.innerHTML = `
+      <img src="${url}" alt="${name}" class="image">
+      <p class="image-name">${name}</p>
+      <button class="delete-btn" data-id="${id}">Delete</button>
+    `;
 
-btn.addEventListener("click", () => {
-  fetch("https://66e7e69bb17821a9d9da6eab.mockapi.io/login", {
-    method: 'POST',
-    body: JSON.stringify({
-      name: input.value,
-      img: img.value,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
+    div.querySelector('.delete-btn').addEventListener('click', (event) => {
+      let button = event.target;
+      let imageId = button.getAttribute('data-id');
+      deleteImage(imageId, div);
+    });
 
-    .then((response) => response.json())
-    .then((data) => {
+    container.appendChild(div);
+  };
 
-      let div = document.createElement("div");
-      let image = document.createElement("img");
-      let name = document.createElement("p");
+  let deleteImage = (id, element) => {
+    fetch(`https://66e7e69bb17821a9d9da6eab.mockapi.io/login/${id}`, { 
+      method: 'DELETE' })
+      .then(response => response.json())
+      .then(() => element.remove())
+      .catch(console.error);
+  };
 
-      image.src = data.img;
-      image.alt = data.name;
-      name.textContent = data.name;
+  let loadImages = () => {
+    fetch("https://66e7e69bb17821a9d9da6eab.mockapi.io/login")
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(imageData => {
+          addImage(imageData.id, imageData.img, imageData.name);
+        });
+      })
+      .catch(console.error);
+  };
 
-      Container.appendChild(name);
-      Container.appendChild(image);
-      imageContainer.appendChild(div);
+  submitBtn.addEventListener("click", () => {
+    fetch("https://66e7e69bb17821a9d9da6eab.mockapi.io/login", {
+      method: 'POST',
+      body: JSON.stringify({
+        name: input.value,
+        img: imgInput.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     })
+    .then(response => response.json())
+    .then(data => {
+      addImage(data.id, data.img, data.name); 
+      input.value = ''; 
+      imgInput.value = '';
+    })
+    .catch(console.error);
+  });
 
+  loadImages();
 });
-
